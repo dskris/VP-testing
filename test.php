4be1e175d7233da5756892b024b33955f2b96b04
kris <kris@digitalsymphony.com>
2018-05-17 16:29:53 +0800

    first commit

---
+++ test.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+$host = 'http://localhost:4444/wd/hub'; // this is the default
+$USE_FIREFOX = true; // if false, will use chrome.
+
+if ($USE_FIREFOX)
+{
+    $driver = Facebook\WebDriver\Remote\RemoteWebDriver::create(
+        $host, 
+        Facebook\WebDriver\Remote\DesiredCapabilities::firefox()
+    );
+}
+else
+{
+    $driver = Facebook\WebDriver\Remote\RemoteWebDriver::create(
+        $host, 
+        Facebook\WebDriver\Remote\DesiredCapabilities::chrome()
+    );
+}
+
+$driver->get("http://www.venuepoint.com.my");
+
+/*# enter text into the search field
+$driver->findElement(Facebook\WebDriver\WebDriverBy::id('lst-ib'))->click();
+sleep(1);
+$driver->findElement(Facebook\WebDriver\WebDriverBy::id('lst-ib'))->sendKeys('programster selenium');
+sleep(1);
+
+# Click the search button
+$driver->findElement(Facebook\WebDriver\WebDriverBy::name('btnK'))->click();*/
+
+$driver->findElement(Facebook\WebDriver\WebDriverBy::id('loginsigin'))->click();
+sleep(1);
+$driver->findElement(Facebook\WebDriver\WebDriverBy::id('email3'))->sendKeys('kris26ooi@gmail.com');
+sleep(1);
+$driver->findElement(Facebook\WebDriver\WebDriverBy::id('pass3'))->sendKeys('horse26');
+sleep(1);
+$driver->findElement(Facebook\WebDriver\WebDriverBy::id('sign1'))->click();
+
